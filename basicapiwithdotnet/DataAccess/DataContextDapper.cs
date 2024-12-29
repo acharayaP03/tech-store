8c1beabe015939be69761a2ebf90408b09ee42e6
acharayaP03 <acharyaP03@gmail.com>
2024-12-29 20:15:43 +1100

    add LoadDataWithParameter methods so that we can execute sql while passing parameter

--- basicapiwithdotnet/DataAccess/DataContextDapper.cs
+++ basicapiwithdotnet/DataAccess/DataContextDapper.cs
@@ -20,10 +20,17 @@ public class DataContextDapper
     public IEnumerable<T> LoadData<T>(string sql) => _dbConnection.Query<T>(sql);
 
     public T LoadDataSingle<T>(string sql) => _dbConnection.QuerySingle<T>(sql);
-   
+    public IEnumerable<T> LoadDataWithParameters<T>(string sql, object parameters) =>  _dbConnection.Query<T>(sql, parameters);
 
     public bool ExecuteSql(string sql) => _dbConnection.Execute(sql) > 0;
 
     public int ExecuteSqlWithRowCount(string sql, object parameters) => _dbConnection.Execute(sql, parameters);
+
+    public T ExecuteScalar<T>(string sql, object? parameters = null)
+    {
+        var result = _dbConnection.ExecuteScalar<T>(sql, parameters) ?? throw new InvalidOperationException("Query returned null.");
+        return result;
+    }
+
     
 }
